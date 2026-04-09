24771110ddb330416b93fba13ff5752fcde21d36
atp-adarshtp <atp@dewdrive.com>
2026-04-09 11:18:51 +0530

    add .env

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
@@ -6,14 +7,14 @@ const cors = require('cors');
 const Anime = require('./models/Anime');
 const Episode = require('./models/Episode');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Database Connection
-const mongoURI = 'mongodb://127.0.0.1:27017/OnePiece'; // Change 'one piece' if you want
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/OnePiece';
 mongoose.connect(mongoURI);
 
 const connection = mongoose.connection;
